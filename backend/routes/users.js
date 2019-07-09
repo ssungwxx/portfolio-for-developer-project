d5f51b6b98a9b82c76c2d5c07d0987d053612582
김성우 <rlatjddn23@gmail.com>
2019-07-09 10:12:50 +0900

    body-parser 미들웨어 추가 및 users post메소드 추가

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,12 +3,48 @@ var router = express.Router();
 var mongoose = require("mongoose");
 var User = require("../model/User");
 
-// Get All Users
+// Get All Users Info
 router.get("/", function(req, res, next) {
   User.find(function(err, users) {
     if (err) return next(err);
     res.json(users);
   });
 });
+
+// Get One User Info
+router.get("/:id", function(req, res, next) {
+  User.findById(req.params.id, function(err, user) {
+    if (err) return next(err);
+    res.json(user);
+  });
+});
+
+// Get All Posts
+
+// Get All Repositories
+
+// Add User
+router.post("/", function(req, res, next) {
+  User.create(req.body, function(err, user) {
+    if (err) return next(err);
+    res.json(user);
+  });
+});
+
+// Add Post
+
+// Add Repository
+
+// Update User
+
+// Update Post
+
+// Update Repository
+
+// Delete User
+
+// Delete Post
+
+// Delete Repository
 
 module.exports = router;
