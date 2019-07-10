fd0da0512aa53eb3d1c86116550c695a860ef925
신승민 <sammiffy77@naver.com>
2019-07-10 15:49:15 +0900

    Post API - Modify GET All / Add GET One, Post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,17 +6,52 @@ var User = require("../model/User");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 
-// Get All Posts
-router.get("/", function(req, res, next) {
-  User.find({}).select({"posts": 1, "_id": 0}).exec(function(err, posts) {
+// Get Specific User's All Posts
+router.get("/:id", function(req, res, next) {
+  User.find({
+    "user_id": req.param.id
+  }).select({
+    "posts": 1,
+    "_id": 0
+  }).exec(function(err, posts) {
     if (err) return next(err);
     res.json(posts);
   });
+  //   var v = posts[0]["posts"][0];
+  //   console.log(v.post_name);
 });
 
-// Get One Post
+// Get Specific User's One Post
+router.get("/:id/:pname", function(req, res, next) {
+  User.findOne({
+      "user_id": req.params.id
+    }, {
+      "posts": {
+        $elemMatch: {
+          "post_name": req.params.pname
+        }
+      }
+    })
+    .exec(function(err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+});
 
 // Add New Post
+// Problem: Object_id가 같이 들어가는 문제 해결해야함
+router.put("/:id", function(req, res, next) {
+  User.updateOne({
+    "user_id": req.param.id
+  }, {
+    $push: {
+      "posts": req.body
+    }
+  }).exec(function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
 
 // Modify Post
 
