7b1aa81caee298f6cf3d9abd963b64d6220769a5
Terpe66 <45963139+Terpe66@users.noreply.github.com>
2019-07-10 17:58:19 +0900

    testprj

--- frontend/testprj/app.js
+++ frontend/testprj/app.js
@@ -9,27 +9,28 @@ const translationClient = new TranslationServiceClient();
 async function translateText() {
     const request = {
         parent: translationClient.locationPath("ssafy-ss2", "global"),
-        contents: ["Hi, JS"],
+        contents: [document.querySelector("html")],
         mimeType: 'text/html', // mime types: text/plain, text/html
         sourceLanguageCode: 'en-US',
         targetLanguageCode: 'ko-KR',
     };
     const [response] = await translationClient.translateText(request);
-    text = response.translations[0].translatedText;
-    console.log(text);
-    // return text;
+    const text = response.translations[0].translatedText;
+    // console.log(text);
+    return text;
 }
 
-// async function getText() {
-//     const text = translateText();
-// }
 
 app.get("/", function (req, res) {
+    res.render("index.html");
     translateText()
-        .then(res.send("test " + text));
+        .then(text => res.send("text " + text))
 });
 
 app.listen(3000, function () {
     console.log("얍!");
 });
 
+
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
