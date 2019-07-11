314259ad4099aefc8bc7159b369d8c15316649cf
이형주 <gudwn8439@naver.com>
2019-07-11 17:13:34 +0900

    Req 2-3]로그인 기능 구현
    Req 2-3]로그인 기능 구현(Header)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <v-app>
-
         <v-content>
             <Header/>
             <Sidebar/>
@@ -29,6 +28,7 @@
     import TopButton from "./components/TopButton";
     import AddFavorite from "./components/AddFavorite";
 
+
     export default {
         name: 'App',
         components: {
@@ -37,11 +37,10 @@
             store,
             Footer,
             Header,
-            Sidebar,
+            Sidebar
         },
         data() {
             return {
-                //
             }
         },
         mounted () {
@@ -49,15 +48,11 @@
             if (userAgent.indexOf("Chrome") === -1 || userAgent.indexOf("Edge") !== -1) {
                 alert("해당 사이트는 크롬에 최적화 되어 있습니다.");
             }
-
-
-
         }
-    }
+  }
 </script>
 
 <style>
-
 
     #app {
         font-family: "Avenir", Helvetica, Arial, sans-serif;
