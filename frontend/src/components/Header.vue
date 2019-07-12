4fa38182cf02631738c32e65335eb66e45f95156
신승민 <sammiffy77@naver.com>
2019-07-12 16:28:50 +0900

    Login Button&Modal Pop-up 구현

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,96 +1,105 @@
 <template>
-    <div class="header">
-        <v-toolbar dark color="success darken-1" fixed>
+<div class="header">
+  <v-toolbar dark color="success darken-1" fixed>
 
-            <v-btn icon href="/">
-                <v-icon>home</v-icon>
-            </v-btn>
+    <v-btn icon href="/">
+      <v-icon>home</v-icon>
+    </v-btn>
 
-            <v-btn>
-              <v-facebook-login app-id="2340768596039099"></v-facebook-login>
-            </v-btn>
-            <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
+    <v-btn>
+      <v-facebook-login app-id="2340768596039099"></v-facebook-login>
+    </v-btn>
+    <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
 
-            <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-            <div class="icons">
-                <router-link :to="port" style="text-decoration: none;">
-                    <v-btn icon>
-                        <v-icon color="yellow">markunread_mailbox</v-icon>
-                    </v-btn>
-                </router-link>
+    <div class="icons">
+      <router-link :to="port" style="text-decoration: none;">
+        <v-btn icon>
+          <v-icon color="yellow">markunread_mailbox</v-icon>
+        </v-btn>
+      </router-link>
 
-                <router-link :to="posts" style="text-decoration: none;">
-                    <v-btn icon href="./Post">
-                        <v-icon color="yellow">description</v-icon>
-                    </v-btn>
-                </router-link>
+      <router-link :to="posts" style="text-decoration: none;">
+        <v-btn icon href="./Post">
+          <v-icon color="yellow">description</v-icon>
+        </v-btn>
+      </router-link>
 
-                <router-link :to="login" style="text-decoration: none;">
+      <Login class="login"></Login>
+
+      <!-- <router-link :to="login" style="text-decoration: none;">
                   <v-btn icon href="./Login">
                   <v-icon color="yellow">perm_identity</v-icon>
                   </v-btn>
-                </router-link>
+                </router-link> -->
 
-            </div>
-        </v-toolbar>
     </div>
+  </v-toolbar>
+</div>
 </template>
 
 <script>
-import { VFBLogin as VFacebookLogin } from 'vue-facebook-login-component'
+import {
+  VFBLogin as VFacebookLogin
+} from 'vue-facebook-login-component'
 
-    export default {
-        name: "Header",
-        data: () => ({
-            title: document.title,
-            port: "/Portfolio",
-            posts: "/Post",
-            login: "/Login",
-            items: [
-                {
-                    title: "perm_identity",
-                    go: "/Login"
-                },
-                {
-                    title: "description",
-                    go: "/Post"
-                },
-                {
-                    title: "markunread_mailbox",
-                    go: "/Portfolio"
-                },
-            ]
-        }),
-        components: {
-          VFacebookLogin
-        },
-        methods: {
-        }
-    }
+import Login from './Login'
+
+export default {
+  name: "Header",
+  data: () => ({
+    title: document.title,
+    port: "/Portfolio",
+    posts: "/Post",
+    login: "/Login",
+    items: [{
+        title: "perm_identity",
+        go: "/Login"
+      },
+      {
+        title: "description",
+        go: "/Post"
+      },
+      {
+        title: "markunread_mailbox",
+        go: "/Portfolio"
+      },
+    ]
+  }),
+  components: {
+    VFacebookLogin,
+    Login
+  },
+  methods: {}
+}
 </script>
 
 <style>
-    .theme--light.v-list {
-        background: yellow;
-        color: darkgreen;
-        font-weight: bold;
-    }
+.theme--light.v-list {
+  background: yellow;
+  color: darkgreen;
+  font-weight: bold;
+}
 
-    @media screen and (max-width: 600px) {
-        .icons {
-            display: none;
-        }
-    }
+@media screen and (max-width: 600px) {
+  .icons {
+    display: none;
+  }
+}
 
-    .header {
-        z-index: 1;
-        position: relative;
-    }
+.header {
+  z-index: 1;
+  position: relative;
+}
 
-    @media screen and (min-width: 600px) {
-        .menu {
-            display: none;
-        }
-    }
+@media screen and (min-width: 600px) {
+  .menu {
+    display: none;
+  }
+}
+
+.login {
+  display: inline-block;
+}
 </style>
