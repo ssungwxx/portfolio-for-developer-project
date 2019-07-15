6964ec6fb260315476819be2665365fee801a7e9
jisun1002 <45963130+jisun1002@users.noreply.github.com>
2019-07-15 13:58:57 +0900

    markdownEditor 정리

--- frontend/src/views/PortfolioWrite.vue
+++ frontend/src/views/PortfolioWrite.vue
@@ -64,6 +64,7 @@
 
             onFilePicked(e) {
                 const files = e.target.files;
+                console.log(files)
                 if (files[0] !== undefined) {
                     this.imageName = files[0].name
                     if (this.imageName.lastIndexOf('.') <= 0) {
