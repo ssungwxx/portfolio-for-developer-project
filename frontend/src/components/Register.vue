d89fb92ba612d40cac7794e072366c2f65a0ef91
신승민 <sammiffy77@naver.com>
2019-07-16 11:27:12 +0900

    no message

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,43 +1,45 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="dialog" max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Join Us</v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">Register</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Full name*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="ID*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click="signUp">Register</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+<v-layout row justify-center>
+  <v-dialog v-model="dialog" max-width="600px">
+    <template v-slot:activator="{ on }">
+      <v-btn color="primary" dark v-on="on">Join Us</v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Register</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-layout wrap>
+            <v-flex xs12 sm6 md4>
+              <v-text-field label="Full name*" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field label="ID*" required></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field label="Password*" type="password" required></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <small>*indicates required field</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+        <v-btn color="blue darken-1" flat @click="signUp">Register</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</v-layout>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      dialog: false
-    })
-  }
+import RestService from "../services/RestService"
+
+export default {
+  data: () => ({
+    dialog: false
+  })
+}
 </script>
