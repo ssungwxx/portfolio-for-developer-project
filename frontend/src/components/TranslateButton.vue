535d2e7ae3cf1c7e5965c78d56e9293c8ea169e2
신승민 <sammiffy77@naver.com>
2019-07-18 16:48:00 +0900

    Change translate button's color & add tooltip

--- frontend/src/components/TranslateButton.vue
+++ frontend/src/components/TranslateButton.vue
@@ -1,30 +1,29 @@
 <template>
-
-    <v-icon color="red" large class="translate" @click="select">g_translate</v-icon>
-
+<v-tooltip top>
+  <v-icon slot="activator" color="rgb(102, 102, 102)" large class="translate" @click="select">g_translate</v-icon>
+  <span>페이지 번역</span>
+</v-tooltip>
 </template>
 
 <script>
+export default {
+  name: "TranslateButton",
+  methods: {
+    select() {
+      const selected = document.querySelectorAll(".v-content")[1];
 
-    export default {
-        name: "TranslateButton",
-        methods: {
-            select () {
-                const selected = document.querySelectorAll(".v-content")[1];
-                
-            }
-        }
     }
-
+  }
+}
 </script>
 
 <style>
-    .translate {
-        position: fixed;
-        bottom: 50px;
-        left: 50px;
-        margin: 0;
-        padding: 0;
-        z-index: 5;
-    }
+.translate {
+  position: fixed;
+  bottom: 50px;
+  left: 50px;
+  margin: 0;
+  padding: 0;
+  z-index: 5;
+}
 </style>
