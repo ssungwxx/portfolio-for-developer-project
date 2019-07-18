8bcf28ae3b13f6a24ae0dbfb919cead3a68d1cdb
김성우 <rlatjddn23@gmail.com>
2019-07-18 15:43:09 +0900

    Conflict

--- frontend/src/views/PostPage.vue
+++ frontend/src/views/PostPage.vue
@@ -1,25 +1,25 @@
 <template>
-<div>
-  <v-layout>
-    <v-flex xs12>
-      <Header></Header>
-    </v-flex>
-  </v-layout>
-  <!--1번 이미지
+    <div>
+        <v-layout>
+            <v-flex xs12>
+                <Header></Header>
+            </v-flex>
+        </v-layout>
+        <!--1번 이미지
         https://ifh.cc/g/dAPdA.jpg
         -->
-  <ImgBanner imgSrc="https://ifh.cc/g/rNlxw.jpg">
-    <div style="line-height:1.2em;font-size:1.2em;" slot="text">Post</div>
-  </ImgBanner>
-  <v-container>
-    <!-- Post -->
-    <v-layout>
-      <v-flex xs12>
-        <PostList :limits="6" :load-more="true"></PostList>
-      </v-flex>
-    </v-layout>
-  </v-container>
-</div>
+        <ImgBanner imgSrc="https://ifh.cc/g/rNlxw.jpg">
+            <div style="line-height:1.2em;font-size:1.2em;" slot="text">Post</div>
+        </ImgBanner>
+        <v-container>
+            <!-- Post -->
+            <v-layout>
+                <v-flex xs12>
+                    <PostList :limits="6" :load-more="true"></PostList>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
 </template>
 
 <script>
@@ -28,28 +28,13 @@ import PostList from "../components/PostList";
 import RestService from "@/services/RestService";
 
 export default {
-  name: "PostPage",
-  data() {
-    return {
-      home: "/"
-    };
-  },
+    name: "PostPage",
+    data() {
+        return {
+            home: "/"
+        };
+    },
 
-<<<<<<< HEAD
-  beforeMount() {
-    this.insertLog();
-  },
-  components: {
-    ImgBanner,
-    PostList
-  },
-  methods: {
-    async insertLog() {
-      this.insertLog = await RestService.insertLog("PostPage");
-    }
-  }
-}
-=======
     beforeMount() {
         this.insertLog();
     },
@@ -63,5 +48,4 @@ export default {
         }
     }
 };
->>>>>>> 558381c37f642f836e917fed3fd32d0db48d2f66
 </script>
