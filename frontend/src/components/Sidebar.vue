2b0304317ca029362224c03f681f8d7bfdcd4f19
jisun1002 <45963130+jisun1002@users.noreply.github.com>
2019-07-19 09:42:01 +0900

    사이드바 북마크 기능 수정

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -4,7 +4,7 @@
           <v-btn slot="activator" icon >
             <v-tooltip left>
               <v-icon slot="activator" color="white">menu</v-icon>
-              <span> 메뉴</span>
+              <span>메뉴</span>
             </v-tooltip>
           </v-btn>
         <div>
@@ -22,7 +22,18 @@
 <!--      </v-list-tile>-->
 <!--    </v-list>-->
 
-        <v-list style="overflow: hidden;">
+        <v-list class="menuButton" style="overflow: hidden;">
+
+            <v-list-tile @click="favorite()">
+                <v-list-tile-action >
+<!--                    <v-btn slot="activator" icon v-on:click="favorite()">-->
+                        <v-icon>star</v-icon>
+<!--                    </v-btn>-->
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Bookmark</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
           <v-list-tile
                   v-for="(item, i) in items"
                   :key="i"
@@ -105,7 +116,7 @@ export default {
     data: () => ({
         title: document.title,
         items: [
-            { title: 'Bookmark', icon: 'star', url: './bookmark'},
+            // { title: 'Bookmark', icon: 'star', url: './bookmark'},
             { title: 'Post', icon: 'description', url: './post' },
             { title: 'Portfolio', icon: 'assignment_ind', url: './portfolio' },
             // { title: 'Login', icon: 'face', url: './login'},
@@ -117,6 +128,36 @@ export default {
         text: {type: String}
     },
     methods: {
+        favorite() {
+            var bookmarkURL = window.location.href;
+            var bookmarkTitle = document.title;
+            var triggerDefault = false;
+            if (window.sidebar && window.sidebar.addPanel) {
+                window.sidebar.addPanel(bookmarkTitle, bookmarkURL, "");
+            } else if (
+                (window.sidebar &&
+                    navigator.userAgent.toLowerCase().indexOf("firefox") >
+                    -1) ||
+                (window.opera && window.print)
+            ) {
+                var $this = $(this);
+                $this.attr("href", bookmarkURL);
+                $this.attr("title", bookmarkTitle);
+                $this.attr("rel", "sidebar");
+                $this.off(e);
+                triggerDefault = true;
+            } else if (window.external && "AddFavorite" in window.external) {
+                window.external.AddFavorite(bookmarkURL, bookmarkTitle);
+            } else {
+                alert(
+                    (navigator.userAgent.toLowerCase().indexOf("mac") != -1
+                        ? "Cmd"
+                        : "Ctrl") +
+                    "+D 키를 눌러 즐겨찾기에 등록하실 수 있습니다."
+                );
+            }
+            return triggerDefault;
+        }
     },
 }
 </script>
@@ -137,6 +178,9 @@ export default {
 
 @media screen and (min-width: 600px) {
   .sidebar {
+    display: none;
+  }
+  .menuButton {
     display: none;
   }
 }
