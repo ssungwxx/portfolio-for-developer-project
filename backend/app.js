4b2a3ac222549a54426c80f9ad43c68e602b2784
김성우 <rlatjddn23@gmail.com>
2019-07-26 10:40:46 +0900

    cors회피용 코드 주석 삭제

--- backend/app.js
+++ backend/app.js
@@ -31,18 +31,6 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// CORS 회피용
-/*
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET", "POST");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, content-type, Authorization"
-  );
-});
-*/
-
 app.use(
     bodyParser.urlencoded({
         extended: false
