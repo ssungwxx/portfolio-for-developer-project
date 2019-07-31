e37a95fc68512a36d4a0bcb2a9f8ec99b0a007e5
이원진 <terpe66@gmail.com>
2019-07-31 15:54:43 +0900

    insertPost 수정

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -15,7 +15,7 @@
             <v-btn class="target" style="margin-right: auto;" color="#ffc0cb" dark v-on:click="loadMorePosts">
                 <v-icon size="25" class="mr-2">fa-plus</v-icon>더 보기
             </v-btn>
-            <v-btn class="target" style="margin-right: auto; margin-top: 3rem" color="#ffc0cb" href="/writepost" dark>
+            <v-btn class="target" style="margin-right: auto; margin-top: 3rem" color="#ffc0cb" :user_id="user_id" href="/writepost" dark>
                 <v-icon size="25" class="mr-2">fa-edit</v-icon>Write
             </v-btn>
         </div>
--- frontend/src/views/WritePost.vue
+++ frontend/src/views/WritePost.vue
@@ -1,45 +1,55 @@
 <template>
     <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
-        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+        <v-text-field v-model="title" :counter="10" :rules="titleRules" label="제목" required></v-text-field>
+        <v-text-field v-model="content" :rules="contentRules" label="내용" required></v-text-field>
 
-        <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
-        <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
-        <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">쓰기</v-btn>
+        <v-btn color="error" class="mr-4" @click="reset">초기화</v-btn>
     </v-form>
 </template>
 
 <script>
+import RestService from "@/services/RestService"
+
 export default {
     name: "WritePost",
+    props: {
+        user_id: {type: String}
+    },
     data() {
         return {
             valid: true,
-            name: '',
-            nameRules: [
-                v => !!v || 'Name is required',
-                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            title: "",
+            titleRules: [
+                v => !!v || 'Title is required',
+                // v => (v && v.length <= 10) || 'Name must be less than 10 characters',
             ],
-            email: '',
-            emailRules: [
-                v => !!v || 'E-mail is required',
-                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            content: "",
+            contentRules: [
+                v => !!v || 'Content is required',
+                // v => (v && v.length <= 10) || 'Name must be less than 10 characters',
             ],
-            select: null,
         }
     },
     methods: {
         validate () {
             if (this.$refs.form.validate()) {
-                this.snackbar = true
+                const data = {
+                    user_id: this.user_id,
+                    post_title: this.title,
+                    post_content: this.content,
+                    post_date: new Date(),
+                    post_cnt: 0,
+                };
+                this.postpost(data);
             }
         },
         reset () {
             this.$refs.form.reset()
         },
-        resetValidation () {
-            this.$refs.form.resetValidation()
-        },
+        async postpost(data) {
+            await RestService.insertPost(data);
+        }
     },
 }
 </script>
