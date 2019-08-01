d0fccb9888922a98313c5628411e14c3c83dfc11
이원진 <terpe66@gmail.com>
2019-08-01 12:08:03 +0900

    Update posts.js

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -25,6 +25,13 @@ router.get("/count/:id", (req, res) => {
         .then(data => res.json(data));
 });
 
+router.get("/user/:id", (req, res) => {
+    knex("posts")
+        .select("*")
+        .where("user_id", req.params.id)
+        .then(data => res.json(data));
+});
+
 // Get one detailed post
 router.get("/:id", (req, res) => {
     knex("posts")
