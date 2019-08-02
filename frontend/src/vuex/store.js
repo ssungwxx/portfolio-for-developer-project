9848960268a076870fd3aa0f4b9f4dea5a3cef5f
신승민 <sammiffy77@naver.com>
2019-08-02 16:29:54 +0900

    Add additional actions to store.js

--- frontend/src/vuex/store.js
+++ frontend/src/vuex/store.js
@@ -6,16 +6,34 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    isAuth: false
+    isAuth: false,
+    user_id: null,
+    err_msg: null
   },
   getters: {
     getIsAuth: state => {
       return state.isAuth;
+    },
+    getUser_id: state => {
+      return state.user_id;
+    },
+    getErr_msg: state => {
+      return state.err_msg;
     }
   },
   mutations: {
-    login(state, isAuth) {
-      state.isAuth = isAuth;
+    login(state, data) {
+      state.isAuth = true;
+      state.user_id = data.user_id;
+      state.err_msg = null;
+    },
+    logout(state) {
+      state.isAuth = false;
+      state.user_id = null;
+      state.err_msg = null;
+    },
+    setErr_msg(state, msg) {
+      state.err_msg = msg;
     }
   },
   actions: {
@@ -26,7 +44,9 @@ export const store = new Vuex.Store({
       var result = await RestService.loginUser(data);
       console.log(result);
       if (result.status == 200) {
-        commit('login', true);
+        commit('login', data);
+      } else {
+        commit('setErr_msg', result.msg);
       }
     }
   }
