2d533e80b0b1f2ddcc6fb1b9303dc4d0ef3cb61e
김성우 <rlatjddn23@gmail.com>
2019-08-07 09:38:02 +0900

    repo comment token 판별 추가

--- backend/routes/repo_comment.js
+++ backend/routes/repo_comment.js
@@ -21,24 +21,213 @@ router.get("/:user_id", (req, res) => {
 
 // Delete one comment
 router.delete("/:rcom_no", (req, res) => {
-    knex("repositories_comment")
-        .delete()
-        .where("repo_no", req.params.repo_no)
-        .then(data => res.json(data));
+    // Token Refresh 함수
+    async function refresh() {
+        const result = await getRefreshToken(token.user_id);
+        try {
+            refresh_token = jwt.verify(result, secretObj.refresh);
+            const new_token = jwt.sign(
+                {
+                    user_id: req.body.user_id
+                },
+                secretObj.secret,
+                {
+                    expiresIn: "5m"
+                }
+            );
+            token = jwt.verify(new_token, secretObj.secret);
+
+            knex("users")
+                .where("user_id", req.body.id)
+                .delete()
+                .then(data =>
+                    res.json({
+                        status: 200,
+                        msg: "success"
+                    })
+                );
+        } catch {
+            token.exp = refresh.exp;
+        }
+    }
+
+    // 헤더에 jwt가 넘어오지 않을 때
+    if (!req.headers.jwt) {
+        res.json({
+            status: 400,
+            msg: "no jwt in session"
+        });
+        return;
+    }
+
+    // Token 검증
+    try {
+        token = jwt.verify(req.headers.jwt, secretObj.secret);
+    } catch {
+        token = {
+            user_id: req.body.user_id,
+            exp: -1
+        };
+    }
+
+    if ((token.exp = -1)) {
+        refresh();
+    } else if (token.exp < Date.now) {
+        knex("repositories_comment")
+            .delete()
+            .where("repo_no", req.params.repo_no)
+            .then(data =>
+                res.json({
+                    status: 200,
+                    msg: "success"
+                })
+            );
+    } else {
+        res.json({
+            status: 400,
+            msg: "error"
+        });
+    }
 });
 
 // Insert One comment
 router.post("/", (req, res) => {
-    knex("repositories_comment")
-        .insert(req.body)
-        .then(data => res.json(data));
+    // Token Refresh 함수
+    async function refresh() {
+        const result = await getRefreshToken(token.user_id);
+        try {
+            refresh_token = jwt.verify(result, secretObj.refresh);
+            const new_token = jwt.sign(
+                {
+                    user_id: req.body.user_id
+                },
+                secretObj.secret,
+                {
+                    expiresIn: "5m"
+                }
+            );
+            token = jwt.verify(new_token, secretObj.secret);
+
+            knex("users")
+                .where("user_id", req.body.id)
+                .delete()
+                .then(data =>
+                    res.json({
+                        status: 200,
+                        msg: "success"
+                    })
+                );
+        } catch {
+            token.exp = refresh.exp;
+        }
+    }
+
+    // 헤더에 jwt가 넘어오지 않을 때
+    if (!req.headers.jwt) {
+        res.json({
+            status: 400,
+            msg: "no jwt in session"
+        });
+        return;
+    }
+
+    // Token 검증
+    try {
+        token = jwt.verify(req.headers.jwt, secretObj.secret);
+    } catch {
+        token = {
+            user_id: req.body.user_id,
+            exp: -1
+        };
+    }
+
+    if ((token.exp = -1)) {
+        refresh();
+    } else if (token.exp < Date.now) {
+        knex("repositories_comment")
+            .insert(req.body)
+            .then(data =>
+                res.json({
+                    status: 200,
+                    msg: "success"
+                })
+            );
+    } else {
+        res.json({
+            status: 400,
+            msg: "error"
+        });
+    }
 });
 
 // Update One comment
 router.put("/:rcom_no", (req, res) => {
-    knex("repositories_comment")
-        .insert(req.body)
-        .then(data => res.json(data));
+    // Token Refresh 함수
+    async function refresh() {
+        const result = await getRefreshToken(token.user_id);
+        try {
+            refresh_token = jwt.verify(result, secretObj.refresh);
+            const new_token = jwt.sign(
+                {
+                    user_id: req.body.user_id
+                },
+                secretObj.secret,
+                {
+                    expiresIn: "5m"
+                }
+            );
+            token = jwt.verify(new_token, secretObj.secret);
+
+            knex("users")
+                .where("user_id", req.body.id)
+                .delete()
+                .then(data =>
+                    res.json({
+                        status: 200,
+                        msg: "success"
+                    })
+                );
+        } catch {
+            token.exp = refresh.exp;
+        }
+    }
+
+    // 헤더에 jwt가 넘어오지 않을 때
+    if (!req.headers.jwt) {
+        res.json({
+            status: 400,
+            msg: "no jwt in session"
+        });
+        return;
+    }
+
+    // Token 검증
+    try {
+        token = jwt.verify(req.headers.jwt, secretObj.secret);
+    } catch {
+        token = {
+            user_id: req.body.user_id,
+            exp: -1
+        };
+    }
+
+    if ((token.exp = -1)) {
+        refresh();
+    } else if (token.exp < Date.now) {
+        knex("repositories_comment")
+            .insert(req.body)
+            .then(data =>
+                res.json({
+                    status: 200,
+                    msg: "success"
+                })
+            );
+    } else {
+        res.json({
+            status: 400,
+            msg: "error"
+        });
+    }
 });
 
 module.exports = router;
