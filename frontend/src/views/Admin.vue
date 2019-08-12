a5b15ba66d6fee05c58d939ad6e97f7d2b90f24c
이원진 <terpe66@gmail.com>
2019-08-12 19:43:28 +0900

    admin page 수정

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,33 +1,73 @@
 <template>
     <div>
-        <div class="side">
-            <sidebar-menu :menu="menu" class="menubar"/>
+        <div style="display: flex; justify-content: flex-end;">
+            <v-btn class="adminbutton" @click="focus = 'users'">Users</v-btn>
+            <v-btn class="adminbutton" @click="focus = 'logs'">Logs</v-btn>
+            <v-btn class="adminbutton" @click="focus = 'delegate'">Delegate</v-btn>
         </div>
-        <div class="countinfo">
-            <table class="table">
+        <div v-if="focus === 'users'">
+            <div class="countinfo">
+                <table class="table">
+                    <tr>
+                        <th style="text-align: center;">User_ID</th>
+                        <th style="text-align: center;">Posts</th>
+                        <th style="text-align: center;">Repositories</th>
+                    </tr>
+                    <tr v-for="user of users">
+                        <td>{{ user[0] }}</td>
+                        <td>{{ user[1] }}</td>
+                        <td>{{ user[2] }}</td>
+                    </tr>
+                    <tr class="total">
+                        <td>Total</td>
+                        <td>{{ post_cnt[0].cnt }}개</td>
+                        <td>{{ repo_cnt[0].cnt }}개</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <v-layout mt-5 wrap v-else-if="focus === 'logs'">
+            <v-flex v-for="log in Logs" >
+                {{log.wlog_no}}
+                <br />
+                {{log.user_id}}
+                <br />
+                {{log.wlog_path}}
+                <br />
+                {{log.wlog_date}}
+            </v-flex>
+        </v-layout>
+        <v-layout my-5 v-else>
+            <table>
+                <thead>
                 <tr>
-                    <th style="text-align: center;">User_ID</th>
-                    <th style="text-align: center;">Posts</th>
-                    <th style="text-align: center;">Repositories</th>
+                    <th class="title">ID</th><th class="title">NICKNAME</th><th class="title">GRADE NUM</th><th class="title">VISITOR</th><th class="title">MEMBER</th>
                 </tr>
-                <tr v-for="user of users">
-                    <td>{{ user[0] }}</td>
-                    <td>{{ user[1] }}</td>
-                    <td>{{ user[2] }}</td>
+                </thead>
+                <tbody>
+                <tr  v-for="(user, i) in userList" v-if="user.user_grade!==10">
+                    <td class="userdata">{{user.user_id}}</td><td class="userdata">{{user.user_name}}</td>
+                    <td class="userdata">{{user.user_grade}}</td>
+                    <td class="userdata">
+                        <select class="customselect" id="`TestSelect${i}`" name="SelectValue" @change="updateUser(i, user.user_id)">
+                            <option value="0" selected v-if="user.user_grade === 0">방문자</option>
+                            <option value="0" v-else>방문자</option>
+                            <option value="5" selected v-if="user.user_grade === 5">팀원</option>
+                            <option value="5" v-else>팀원</option>
+                        </select>
+                    </td>
                 </tr>
-                <tr class="total">
-                    <td>Total</td>
-                    <td>{{ post_cnt[0].cnt }}개</td>
-                    <td>{{ repo_cnt[0].cnt }}개</td>
-                </tr>
+                </tbody>
             </table>
-        </div>
+
+        </v-layout>
     </div>
 </template>
 
 
 <script>
     import RestService from "@/services/RestService";
+    import AdminRestService from "@/services/AdminRestService";
 
     export default {
         name: "Admin",
@@ -35,11 +75,14 @@
             this.count();
             this.allcnt();
             this.insertLog();
+            this.getLog();
+            this.getUsers();
         },
         components: {},
         data() {
             return {
                 users: [],
+                userList: [],
                 userPosts: [],
                 userRepos: [],
                 menu: [
@@ -76,6 +119,8 @@
                     cnt: ""
                 }],
                 check: false,
+                focus: "users",
+                Logs: [],
             };
         },
         methods: {
@@ -100,6 +145,19 @@
             async allcnt() {
                 this.users = await RestService.getCount()
             },
+            async getLog() {
+                this.Logs = await RestService.getLog();
+            },
+            async getUsers() {
+                this.userList = await AdminRestService.getUsers();
+            },
+            async updateUser(i, id){
+                const newGrade = document.getElementById(`TestSelect${i}`);
+                const data = {
+                    "user_grade" : newGrade.options.value
+                };
+                await AdminRestService.updateUser(id, selectedValue);
+            },
         },
         computed: {
             getIsLogin: function () {
@@ -116,6 +174,11 @@
 </script>
 
 <style>
+    .adminbutton {
+        margin-left: 0px !important;
+        background-color: transparent !important;
+    }
+
     .side {
         float: left;
         height: auto;
