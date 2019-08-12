71ed177209c621cf57b3165e52280ca0851edc32
김성우 <rlatjddn23@gmail.com>
2019-08-12 19:20:22 +0900

    배포 전 에러사항 수정

--- frontend/src/config/DBConnection.js
+++ frontend/src/config/DBConnection.js
@@ -1,8 +1,8 @@
 let DBConnect = {};
 
-DBConnect.URL = "http://70.12.246.138:3000";
+DBConnect.URL = "http://54.180.158.137:3000";
 DBConnect.fcm_auth_token =
     "key=AAAAv_NYWa4:APA91bEv_8joSyJhsPqPh0tPA1-6-IMN01sSZ1d-N8vTHyaSOGRBpRa67GhXEDDi-yi5lOCiBpuyoUWJLcMiqQx_iWBihl66NHTtKM22kY_WpEwc8CcUyaJU4TfzwEJWZQ6pktzD8YaL";
-DBConnect.adminURL = "http://70.12.246.138:3000/admin";
+DBConnect.adminURL = "http://54.180.158.137:3000/admin";
 
 module.exports = DBConnect;
